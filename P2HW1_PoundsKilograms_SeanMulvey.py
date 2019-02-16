e69bc2bd5a104cababa3b721730318bb3d586d3b
mulveys7619 <47682134+mulveys7619@users.noreply.github.com>
2019-02-15 23:32:53 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_SeanMulvey.py
@@ -0,0 +1,26 @@
+
+# Converts weight from pounds to kilograms
+#02/11/2019
+#CTI-110 P2HW1-Pounds to Kilograms Converter
+#Sean Mulvey
+########################################################################
+
+
+print ("This converts your weight from pounds(lb.) to Kilograms (kg.)")
+    # Tells user what the code is meant to do
+
+print ()
+
+pound = input ("What is your weight in pounds (lb.)? : ")
+    # Asks user to input weight in pounds
+
+kilo = float (pound) / 2.2046
+    # Defines variable for pound to kilo conversion
+
+print()
+
+
+print ("your weight in Kilograms (kg.) is : ", kilo)
+    # Tells user what their weight in Kg. is
+
+
