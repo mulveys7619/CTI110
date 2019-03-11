a38501b07478c1bc3f6b56449a929a55c5b6b5ba
mulveys7619 <47682134+mulveys7619@users.noreply.github.com>
2019-03-10 22:03:24 -0400

    Add files via upload

---
+++ P5T1_KilometerConverter_SeanMulvey.py
@@ -0,0 +1,20 @@
+# This code converts kilometers to miles
+# 03/10/2019
+# CTI-110 P5T1_KilometerConverter
+# Sean Mulvey
+##################################################
+print("*****This code converts kilometers into miles*****")
+    # Tells the user what the program does
+
+print()
+
+kilo = float (input("Please enter the distance of kilometers: "))
+    # Asks user to input a value for kilometers
+print()
+
+mile = kilo * .6214
+    # Sets the equation to convert from kilometers to miles
+
+print(kilo," kilometers is equal to ", format(mile,".2f")," miles")
+    # Displays the result
+              
